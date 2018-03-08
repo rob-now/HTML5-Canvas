e9013084c6ffd28b8649d2853c056c53056b65dc
rob-now <rnowakowski.prv@gmail.com>
2018-03-08 16:23:34 +0100

    Drawing a robot

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+context.strokeStyle = "#88F";
+
 context.beginPath();
-context.fillStyle = "#888";
-context.fillRect(100,200,200,300);
 context.arc(200,200,100,Math.PI,2*Math.PI);
-
-
 context.stroke();
+
+
+context.strokeRect(100,200,200,190);
+context.fillStyle = "#88A";
+context.fillRect(120,220,60,60);
+context.fillRect(220,220,60,60);
+context.font="30px Arial";
+context.fillText("I am Robot on Canvas",50,60);
