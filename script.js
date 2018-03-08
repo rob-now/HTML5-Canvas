5d7b462f8eb8f29b81033fc6a617ec4732665d83
rob-now <rnowakowski.prv@gmail.com>
2018-03-08 15:54:06 +0100

    Drawing arc and rectangle

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+context.beginPath();
+context.fillStyle = "#888";
+context.fillRect(100,200,200,300);
+context.arc(200,200,100,Math.PI,2*Math.PI);
+
+
+context.stroke();
